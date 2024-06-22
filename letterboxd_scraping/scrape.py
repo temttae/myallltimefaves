740569126272937d370173dc47be0f9298b6d822
Tem Taepaisitphongse <temt4510@gmail.com>
2024-06-22 14:19:56 +0700

    scrape film data from letterboxd

---
+++ letterboxd_scraping/scrape.py
@@ -0,0 +1,71 @@
+import requests
+from bs4 import BeautifulSoup
+
+import pandas as pd
+
+username = '_temtae'
+
+def scrape_film(link):
+
+  URL = f'https://letterboxd.com/film/{link}/'
+  page = requests.get(URL)
+
+  soup = BeautifulSoup(page.content, 'html.parser')
+
+  # film header
+  header = soup.find('section', class_='film-header-group')
+
+  title = header.find('h1', class_='filmtitle').find('span').text
+  year = header.find('div', class_='releaseyear').find('a').text
+  directorlist = []
+  directors = header.find('span', class_='directorlist').find_all('a', class_='contributor')
+  for director in directors:
+    directorlist.append(director.text)
+
+  # details
+  runtime = soup.find('p', class_='text-link text-footer').text.strip().split(' ')[0][:-5] # in minutes
+
+  genrelist = []
+  genres = soup.find('div', id='tab-genres').find('div').find_all('a')
+  for genre in genres:
+    genrelist.append(genre.text)
+
+  castlist = {}
+  castmembers = soup.find('div', id='tab-cast').find('div').find_all('a')
+  for member in castmembers: # limit number of members to display in html
+    actor = member.text
+    character = member.get('title')
+    castlist.update({actor: character})
+
+  synopsis = soup.find('div', class_='review body-text -prose -hero prettify')
+  tagline = synopsis.find('h4').text
+  summary = synopsis.find('p').text
+
+  # rating
+  SUBURL = f'https://letterboxd.com/{username}/film/{link}/activity'
+  subpage = requests.get(SUBURL)
+  subsoup = BeautifulSoup(subpage.content, 'html.parser')
+
+  ratingstars = subsoup.find('div', class_='activity-table').find('p', class_='activity-summary').find('span', class_='rating').text.strip()
+  rating = len(ratingstars)
+  if ratingstars[-1] == '½':
+    rating -= 0.5
+  
+  # print(film, title, year, directorlist, runtime, genrelist, castlist, tagline, summary, rating)
+  data = {'link': link, 'title': title, 'year': year, 'directorlist': directorlist, 'runtime': runtime, 
+          'genrelist': genrelist, 'castlist': castlist, 'tagline': tagline, 'summary': summary, 'rating': rating}
+  df = pd.DataFrame([data])
+
+  return df
+
+
+# scrape data from films
+df = pd.DataFrame(columns=['link', 'title', 'year', 'directorlist', 'runtime', 'genrelist', 'castlist', 'tagline', 'summary', 'rating'])
+
+links = ['good-will-hunting']
+
+for link in links:
+  row = scrape_film(link)
+  df = pd.concat([df, row], ignore_index=True)
+
+df.to_json('./data.json', orient='records', indent=4)
