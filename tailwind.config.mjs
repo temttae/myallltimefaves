eb9382cc6f21ac033666cb9f0835478c1321d765
Tem Taepaisitphongse <temt4510@gmail.com>
2024-06-23 22:03:59 +0700

    setup basic styles, create base files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    colors: {
+      'white': '#ffffff',
+      'black': '#252525',
+      'lightgrey': '#e8e7e6',
+      'darkgrey': '#d9d9d9',
+      'yellow': '#e0a800',
+    },
+    fontFamily: {
+      sans: ['Proza Libre', 'sans-serif'],
+      serif: ['Spectral', 'serif'],
+    },
 		extend: {},
 	},
 	plugins: [],
